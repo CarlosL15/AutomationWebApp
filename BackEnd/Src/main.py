7cfa73a6fff5d0b2be6ae8c735fc2602431d53c1
Carlos Lara <carloslara8426@gmail.com>
2025-12-17 15:37:03 -0500

    Added more to the backend and connected the backend to the data base

--- BackEnd/Src/main.py
+++ BackEnd/Src/main.py
@@ -1,7 +1,24 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException, Depends
+from pydantic import BaseModel
+from typing import List, Annotated
+from database import engine, SessionLocal
+from sqlalchemy.orm import Session
+
 
 app = FastAPI()
 
-@app.get("/")
-def index():
-    return {"message": "Hello, barlo!"}
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
+
+db_dependency = Annotated[Session, Depends(get_db)]
+
+@app.post("/users")
+async def create_user():
+    return {"message": "User created"}
+
+
---
+++ README.md
@@ -0,0 +1,2 @@
+#Social Media Automation Website
+I am working on a website that gives you social media automation similar to apps like sprout social. I will be updating this frequently. 
